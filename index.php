ff06fbd0e9afbbe4f63203dd9213c29bb2ac422b
Iwan Vos <Iwan>
2023-03-11 01:50:38 +0100

    done?

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 <body>
 	<form method="post">
 		<label for="total">Totaal (Bakken)</label>
-		<input type="number" id="total" name="total" required><br>
+		<input type="number" id="total" name="total" required value="<?php echo isset($_POST['total']) ? $_POST['total'] : ''; ?>"> <br>
 
 		<label for="pallets">Aantal Stapels Per Pallet</label>
-		<input type="number" id="pallets" name="pallets" value="1" required><br>
+		<input type="number" id="pallets" name="pallets" required value="<?php echo isset($_POST['pallets']) ? $_POST['pallets'] : 1; ?>"><br>
 
 		<label for="max_height">Max Hoogte</label>
-		<input type="number" id="max_height" name="max_height" required><br>
+		<input type="number" id="max_height" name="max_height" required value="<?php echo isset($_POST['max_height']) ? $_POST['max_height'] : 13; ?>"><br>
 
 		<input type="submit" name="submit" value="Submit">
 	</form>
@@ -28,29 +28,27 @@
 		$max_height = $_POST['max_height'];
 		$max_height_cal = $max_height * $row_per_pallet;
 
-		$num_pallets = ceil($total / $max_height_cal); // Calculate the total number of pallets needed
+		$num_pallets = ceil($total / $max_height_cal); 
 	
-		$pallets = array_fill(0, $num_pallets, 0); // Initialize an array to hold the number of boxes on each pallet
+		$pallets = array_fill(0, $num_pallets, 0); 
 	
 		for ($i = 0; $i < $total; $i++) {
-			$min_pallet_height = min($pallets); // Find the pallet with the smallest height
-			$pallets[array_search($min_pallet_height, $pallets)] += 1; // Add the box to that pallet
+			$min_pallet_height = min($pallets); 
+			$pallets[array_search($min_pallet_height, $pallets)] += 1; 
 		}
 
-		// Print the pallet distribution
 		$heights = array_count_values($pallets);
 
-		// Print the pallet distribution
+
 		if ($row_per_pallet == 1) {
 			foreach ($heights as $height => $count) {
-				echo "<div>" . $count . " pallet(s) hoogte " . $height . "<br>" . "</div>";
+				echo "<div>" . $count . " pallet(s) van "  . $height . " hoog <br>" . "</div>";
 			}
 		} else {
+
 			foreach ($heights as $height => $count) {
-				$num_full_rows = floor($height / $row_per_pallet);
-				$num_boxes_on_last_row = $height % $row_per_pallet;
-				$total_boxes_on_pallet = $num_full_rows * $row_per_pallet + $num_boxes_on_last_row;
-				echo "<div>" . $count . " pallet(s) hoogte " . $max_height . "<br>" . "per pallet totaal " . $total_boxes_on_pallet . " bakken" . "</div>";
+		
+				echo "<div>" . $count . " pallet(s) van " . $height/$row_per_pallet . " hoog per stapel <br>" . "per pallet totaal " . $height . " bakken" . "</div>";
 			}
 		}
 	}
